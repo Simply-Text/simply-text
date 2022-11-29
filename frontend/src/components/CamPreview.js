f80a4807958ae610b67896b3c1f782a00c82b633
Jeremy Dufour <jeremy.k.dufour@gmail.com>
2022-11-29 17:25:32 -0500

    Added canvas and rectangles

--- frontend/src/components/CamPreview.js
+++ frontend/src/components/CamPreview.js
@@ -1,7 +1,7 @@
 import "./styles/CamPreview.css";
 import Webcam from "react-webcam";
-import React, {useEffect, useRef} from "react";
-import {addPage} from "../utils/firebase"
+import React, { useEffect, useRef } from "react";
+import { addPage } from "../utils/firebase"
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { createWorker } from "tesseract.js";
 
@@ -23,6 +23,7 @@ const callGoogleVisionApi = async (base64) => {
 
 const CamPreview = () => {
   const camPreview = useRef(null);
+  const canvasRef = useRef(null);
   const [url, setUrl] = React.useState(null);
   const [base64, setBase64] = React.useState(null);
   const [extractedText, setExtractedText] = React.useState("No Extracted Text");
@@ -30,13 +31,14 @@ const CamPreview = () => {
   const [facingMode, setFacingMode] = React.useState(FACING_MODE_ENVIRONMENT);
 
   //tesseract
-  const worker = createWorker();
+
   const runTesseract = async (base64, rectangle) => {
     base64 = "data:image/png;base64," + base64;
+    const worker = createWorker();
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize(base64,rectangle);
+    const { data: { text } } = await worker.recognize(base64, rectangle);
     await worker.terminate();
     return text;
   }
@@ -55,27 +57,81 @@ const CamPreview = () => {
         const visionText = result.response.responses[0].fullTextAnnotation.text;
 
         //Tesseract
-        
+
         console.log(result);
 
         //find all rectangles
-        const rects = [];
-        
-        const vertices = result.response.responses[0].fullTextAnnotation.pages[0].blocks[0].boundingBox.vertices;
-        const xMin = Math.min(vertices[0].x,vertices[1].x,vertices[2].x,vertices[3].x);
-        const xMax = Math.max(vertices[0].x,vertices[1].x,vertices[2].x,vertices[3].x);
-        const yMin = Math.min(vertices[0].y,vertices[1].y,vertices[2].y,vertices[3].y);
-        const yMax = Math.max(vertices[0].y,vertices[1].y,vertices[2].y,vertices[3].y);
-        const w = xMax - xMin;
-        const h = yMax - yMin;
+        var rects = [];
+        var tessResult = "";
+        const rawResults = result.response.responses[0].fullTextAnnotation.pages[0].blocks;
 
-        const rect = {rectangle: {top: yMin, left: xMin, width: w, height: h}};
+        for (let i = 0; i < rawResults.length; i++) {
+          const vertices = rawResults[i].boundingBox.vertices;
+          const xMin = Math.min(vertices[0].x, vertices[1].x, vertices[2].x, vertices[3].x);
+          const xMax = Math.max(vertices[0].x, vertices[1].x, vertices[2].x, vertices[3].x);
+          const yMin = Math.min(vertices[0].y, vertices[1].y, vertices[2].y, vertices[3].y);
+          const yMax = Math.max(vertices[0].y, vertices[1].y, vertices[2].y, vertices[3].y);
+          const w = xMax - xMin;
+          const h = yMax - yMin;
 
-        const tessResult = await runTesseract(base64,rect);
+          const rect = { rectangle: { top: yMin, left: xMin, width: w, height: h } };
+          tessResult += " | " + await runTesseract(base64, rect);
+
+          rects.push(rect);
+        }
+
 
         setExtractedText(visionText);
         setTessText(tessResult);
 
+
+
+
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+
+        
+
+        context.strokeStyle = "#00FF00";
+        var image = new Image();
+        image.onload = () => {
+
+
+          context.drawImage(image, 0, 0);
+
+          //find lines
+          //turn to black and white for line detection
+          var width = image.width;
+          var height = image.height;
+          var imgPixels = context.getImageData(0, 0, width, height);
+
+          for (var y = 0; y < height; y++) {
+            for (var x = 0; x < width; x++) {
+              var i = (y * 4) * width + x * 4;
+
+              var total = ((imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) > 415) ? 255 : 0;
+
+              imgPixels.data[i] = total;
+              imgPixels.data[i + 1] = total;
+              imgPixels.data[i + 2] = total;
+            }
+          }
+
+          context.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+
+          //TODO: add line detection
+
+          //draw rectangles
+          for (let i = 0; i < rects.length; i++) {
+            console.log(i);
+            console.log(rects);
+            context.strokeRect(rects[i].rectangle.left, rects[i].rectangle.top, rects[i].rectangle.width, rects[i].rectangle.height);
+          }
+
+          
+
+        }
+        image.src = "data:image/png;base64," + base64;
         await addPage("user", visionText);
       }
     }
@@ -119,6 +175,7 @@ const CamPreview = () => {
       )}
       <p>{extractedText}</p>
       <p>{tessText}</p>
+      <canvas ref={canvasRef} width={360} height={360}></canvas>
     </>
   );
 };
